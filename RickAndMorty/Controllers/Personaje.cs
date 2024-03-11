a98e11b43fed0b3828342c46d68d16fc1d8c6a37
María Julieta Comparan Velasco <91906153+engineermj98@users.noreply.github.com>
2024-03-11 00:58:06 -0600

    Add files via upload

---
+++ RickAndMorty/Controllers/Personaje.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RickAndMorty.Models;
+using System.Diagnostics;
+
+namespace RickAndMorty.Controllers
+{
+    public class Personaje : Controller
+    {
+        private readonly ModeloServicioAPI _apiService;
+
+        public Personaje()
+        {
+            _apiService = new ModeloServicioAPI();
+        }
+        [Route("Personaje/VistaPersonaje")]
+        public async Task<ActionResult> VistaPersonaje()
+        {
+            var characters = await _apiService.GetCharacters();
+            return View(characters);
+        }
+
+
+        public async Task<ActionResult> ShowCharacterDetails()
+        {
+            var character = await _apiService.GetCharacterById(1);
+            return View(character);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+       
+
+    }
+}
+
---
+++ RickAndMorty/Models/ModeloPersonaje.cs
@@ -0,0 +1,19 @@
+﻿namespace RickAndMorty.Models
+{
+    public class ModeloPersonaje : DetallesPersonaje
+    {
+        public int Id { get; set; }
+        public string? name { get; set; }
+        public string? species { get; set; }
+        public string? image { get; set; }
+        public string? status { get; set; }
+
+    }
+
+    public class DetallesPersonaje
+    {
+        public string? gender { get; set; }
+
+
+    }
+}
