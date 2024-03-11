b75c7c94d35f55aa1ed28e04eb83a1460ba03bc9
María Julieta Comparan Velasco <91906153+engineermj98@users.noreply.github.com>
2024-03-11 00:55:19 -0600

    Add files via upload

---
+++ Models/ModeloServicioAPI.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+
+namespace RickAndMorty.Models
+{
+    public class ModeloServicioAPI
+    {
+        private readonly HttpClient _httpClient;
+
+        public ModeloServicioAPI()
+        {
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri("https://rickandmortyapi.com/api/");
+        }
+
+        public async Task<List<ModeloPersonaje>> GetCharacters()
+        {
+            var response = await _httpClient.GetAsync("character");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonString = await response.Content.ReadAsStringAsync();
+                var characters = JsonConvert.DeserializeObject<ApiResponse<ModeloPersonaje>>(jsonString);
+                return characters?.Results;
+            }
+            return null;
+        }
+
+        public async Task<ModeloPersonaje?> GetCharacterById(int characterId)
+        {
+            var response = await _httpClient.GetAsync($"character/{characterId}");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonString = await response.Content.ReadAsStringAsync();
+                var character = JsonConvert.DeserializeObject<ModeloPersonaje>(jsonString);
+                return character;
+            }
+            return null;
+        }
+    }
+
+    public class ApiResponse<T>
+    {
+        public List<T>? Results { get; set; }
+    }
+
+    public class Character
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Species { get; set; }
+    }
+}
